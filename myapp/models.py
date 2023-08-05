741213b68dba1cf246869d99839c0fadbb7cf502
Faraz Naeem Piracha <faraz29103@gmail.com>
2023-08-05 12:26:26 +0500

    models created

--- myapp/models.py
+++ myapp/models.py
@@ -4,10 +4,25 @@ from django.db import models
 
 class Users(models.Model):
     emp_id = models.CharField(primary_key=True, max_length=255)
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    emp_name = models.CharField(max_length=255)
+    emp_addr = models.CharField(max_length=255)
 
-class Location(models.Model):
-    plant_id = models.AutoField(primary_key=True)
-    pla = models.CharField(max_length=255)
-    Address = models.CharField(max_length=255)
+class Locations(models.Model):
+    loc_id = models.AutoField(primary_key=True)
+    loc_name = models.CharField(max_length=255)
+    loc_address = models.CharField(max_length=255)
+
+class Trips(models.Model):
+    travel_id = models.AutoField(primary_key=True)
+    emp_id = models.CharField(max_length=255)
+    travel_from = models.CharField(max_length=255)
+    travel_to = models.CharField(max_length=255)
+    travel_return_to = models.CharField(max_length=255)
+    travel_distance = models.FloatField(max_length=255)
+    cost = models.FloatField(max_length=255)
+
+class Fuel_Prices(models.Model):
+    price_id = models.AutoField(primary_key=True)
+    fuel_type = models.CharField(primary_key=True, defualt="Petrol")
+    emp_id = models.CharField(max_length=255)
+    travel_from = models.CharField(max_length=255)
